ebc7ce41f0ff677d0a25251d090f0bbe940de584
nessawang <126246300+nessawang@users.noreply.github.com>
2023-06-29 15:56:51 +0200

    Update Main.py

--- Main.py
+++ Main.py
@@ -243,8 +243,14 @@ gpt_input = pd.DataFrame(input_data)
 
 # my openai api key
 import secrets
+import openai_secret_manager
+secrets = openai_secret_manager.Secrets()
+assert "openai" in secrets.get_services()
+openai_secrets = secrets.get_secret("openai")
 
-openai.api_key = secrets.get("openai_key")
+openai.api_key = openai_secrets["openai_key"]
+
+#openai.api_key = secrets.get("openai_key")
 
 # ask ChatGPT for the suggestion    
 def ask_GPT(gpt_input):  
